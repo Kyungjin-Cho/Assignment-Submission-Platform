1590541dd02be72169fe9d074bc2b99b5abce20a
Kyungjin Cho <kyungjin.cho.kr@gmail.com>
2023-01-26 21:43:33 +0000

    Set pug as view engine

--- src/server.js
+++ src/server.js
@@ -12,12 +12,19 @@ import videoRouter from "./routers/videoRouter";
 // Set the PORT as 4000
 const PORT = 4000;
 
+console.log(process.cwd());
+
 // Create express application(server)
 const app = express();
 
 // Create logger middleware(development: GET, path, status code, etc.)
 // Middleware is informing console website user would like to go
 const logger = morgan("dev");
+
+// Set pug as view engine to return html
+app.set("view engine", "pug");
+// Set default value
+app.set("views", process.cwd() + "/src/views");
 
 // Execute controller(middleware)
 app.use(logger);
