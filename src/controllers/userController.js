4d35f979935fc4b88f6396496b935b20d8223116
Kyungjin Cho <kyungjin.cho.kr@gmail.com>
2023-05-31 13:15:23 +0100

    Update controller(status code)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,19 +5,18 @@ export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   const pageTitle = "Join";
   if (password !== password2) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "Password confirmation does not match.",
     });
   }
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "This username/email is already taken.",
     });
   }
-  
   await User.create({
     name,
     username,
@@ -30,5 +29,5 @@ export const postJoin = async (req, res) => {
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
-export const logout = (req,res) => res.send("Logout");
+export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
