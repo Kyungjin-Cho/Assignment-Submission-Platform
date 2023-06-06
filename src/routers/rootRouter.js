2992830551dc68aca7e8968655dd9c6b59ad74d1
Kyungjin Cho <kyungjin.cho.kr@gmail.com>
2023-06-06 14:32:32 +0100

    Update middleware and apply it to routers

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -7,13 +7,18 @@ import {
   postLogin,
 } from "../controllers/userController";
 import {home, search} from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 // Declare router
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
 rootRouter.get("/search", search);
 // Export globalRouter
 export default rootRouter;
