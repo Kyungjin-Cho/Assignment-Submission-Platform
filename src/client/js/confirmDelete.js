a0b5c9b0d7acad90df9cc36943a7394f1408b8bd
Kyungjin Cho <kyungjin.cho.kr@gmail.com>
2023-07-15 20:06:26 +0100

    Fix error related to confirmation before deleting video

--- src/client/js/confirmDelete.js
+++ src/client/js/confirmDelete.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const deleteVideo = document.getElementById('delete-video');
+  const deleteVideo = document.querySelector('.video__delete');
 
   if (deleteVideo) {
     deleteVideo.addEventListener('click', function(e) {
       const confirmation = confirm('Are you sure you want to delete this video?');
-
       if (!confirmation) {
-        // Stop the link from being followed if the user did not confirm
         e.preventDefault();
       }
     });
