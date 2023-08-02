b425baf5cb95e1959d8b0037bd18b5be2cee7057
Kyungjin Cho <kyungjin.cho.kr@gmail.com>
2023-08-02 22:44:41 +0100

    Edit the style of note download & update upload format

--- src/middlewares.js
+++ src/middlewares.js
@@ -48,21 +48,9 @@ const noteStorage = multer.diskStorage({
     cb(null, `${basename}-${Date.now()}${ext}`);
   },
 });
-
-const noteFileFilter = (req, file, cb) => {
-  // Allow only PDF and TXT
-  if (file.mimetype === 'application/pdf' || file.mimetype === 'text/plain') {
-    cb(null, true); // Accept the file
-  } else {
-    cb(null, false); // Reject the file
-    cb(new Error('Invalid file type. Only PDF and TXT files are allowed.'));
-  }
-};
-
 export const noteUpload = multer({
   storage: noteStorage,
   limits: {
     fileSize: 5000000,
-  },
-  fileFilter: noteFileFilter
+  }
 });
